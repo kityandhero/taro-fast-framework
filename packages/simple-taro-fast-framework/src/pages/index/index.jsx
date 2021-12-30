1670fdc84dae1c5ec7ae219bcc65a28d37d19381
卢志涛 <kityandhero@126.com>
2021-12-30 19:26:00 +0800

    refactor(simple-taro-fast-framework): update
    -

--- packages/simple-taro-fast-framework/src/pages/index/index.jsx
+++ packages/simple-taro-fast-framework/src/pages/index/index.jsx
@@ -1,24 +1,27 @@
-import { Component } from 'react'
-import { View, Text } from '@tarojs/components'
-import './index.less'
+import { Component } from "react";
+import { View, Text } from "@tarojs/components";
+
+import { formatMoney, notifySuccess } from "taro-fast-framework/es/utils/tools";
+
+import "./index.less";
 
 export default class Index extends Component {
+  componentWillMount() {}
 
-  componentWillMount () { }
+  componentDidMount() {}
 
-  componentDidMount () { }
+  componentWillUnmount() {}
 
-  componentWillUnmount () { }
+  componentDidShow() {}
 
-  componentDidShow () { }
+  componentDidHide() {}
 
-  componentDidHide () { }
-
-  render () {
+  render() {
     return (
-      <View className='index'>
+      <View className="index">
         <Text>Hello world!</Text>
+        <Text>{formatMoney({ number: 1.54 })}</Text>
       </View>
-    )
+    );
   }
 }
