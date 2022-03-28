52b3391700878f3a53e9cb462e1dc3573369bb9a
kityandhero <kityandhero@126.com>
2022-03-28 19:32:40 +0800

    refactor(simple-taro-fast-framework): update

--- packages/simple-taro-fast-framework/src/example/pages/BackTop/index.jsx
+++ packages/simple-taro-fast-framework/src/example/pages/BackTop/index.jsx
@@ -12,20 +12,10 @@ import {
 } from 'taro-fast-component/es/customComponents';
 
 import ContentPageBase from '../../../customComponents/ContentPageBase';
+import SimpleBox from '../../../customComponents/SimpleBox';
 import PropertyBox from '../../../customComponents/PropertyBox';
-import CodeBox from '../../../customComponents/CodeBox';
 
-const config1 = {
-  visible: true,
-  right: 120,
-  opacity: 0.4,
-  circle: false,
-  iconColor: 'red',
-  backgroundColor: '#000',
-  onClick: () => {
-    console.log('click');
-  },
-};
+const config1 = {};
 
 // eslint-disable-next-line no-undef
 definePageConfig({
@@ -46,6 +36,8 @@ export default class Index extends ContentPageBase {
       ...this.state,
       ...{
         visible1: false,
+        header: '样例1',
+        currentConfig: config1,
       },
     };
 
@@ -53,6 +45,50 @@ export default class Index extends ContentPageBase {
 
     this.thresholdDistance = screenHeight / 2;
   }
+
+  establishControlList = () => {
+    return [
+      {
+        header: '样例1',
+        config: config1,
+      },
+      {
+        header: '样例2',
+        config: {
+          right: 120,
+          opacity: 0.4,
+          circle: false,
+          iconColor: 'red',
+          backgroundColor: '#000',
+        },
+      },
+      {
+        header: '样例3',
+        config: {
+          bottom: 400,
+        },
+      },
+    ];
+  };
+
+  buildSimpleItem = ({ key, config, inner }) => {
+    const { visible1 } = this.state;
+
+    return (
+      <BackTop
+        key={key}
+        {...{
+          ...config,
+          ...{
+            visible: visible1,
+            onClick: this.scrollToTop,
+          },
+        }}
+      >
+        {inner}
+      </BackTop>
+    );
+  };
 
   onPageScroll(e) {
     const { visible1 } = this.state;
@@ -78,46 +114,37 @@ export default class Index extends ContentPageBase {
   };
 
   renderContent = () => {
-    const { visible1 } = this.state;
+    const { header, currentConfig, inner } = this.state;
 
     return (
-      <View>
-        <Space direction="vertical" fillWidth>
-          <View
-            style={{
-              height: transformSize(1200),
-              border: 'var(--tfc-2) solid #ccc',
-              backgroundColor: '#ccc',
-            }}
-          >
-            <HorizontalCenterBox>
-              <View>向下滚动</View>
-            </HorizontalCenterBox>
-          </View>
+      <Space direction="vertical" fillWidth>
+        <View
+          style={{
+            height: transformSize(1200),
+            border: 'var(--tfc-2) solid #ccc',
+            backgroundColor: '#ccc',
+          }}
+        >
+          <HorizontalCenterBox>
+            <View>向下滚动</View>
+          </HorizontalCenterBox>
+        </View>
 
-          <CodeBox
-            config={config1}
-            componentName="BackTop"
-            mockChildren={false}
-          />
+        <SimpleBox
+          header={header}
+          config={currentConfig}
+          componentName="BackTop"
+          mockChildren={!!inner}
+          useInnerBox={false}
+          innerBoxCenterMode
+          innerBoxPadding
+          controlBox={this.buildControlBox(this.establishControlList())}
+        >
+          {this.buildSimpleList()}
+        </SimpleBox>
 
-          <PropertyBox config={BackTop.defaultProps} labelWidth={220} />
-
-          <BackTop visible={visible1} onClick={this.scrollToTop} />
-
-          <BackTop
-            visible={visible1}
-            right={120}
-            opacity={0.4}
-            circle={false}
-            iconColor="red"
-            backgroundColor="#000"
-            onClick={this.scrollToTop}
-          />
-
-          <BackTop visible={visible1} bottom={400} onClick={this.scrollToTop} />
-        </Space>
-      </View>
+        <PropertyBox config={BackTop.defaultProps} labelWidth={240} />
+      </Space>
     );
   };
 }
