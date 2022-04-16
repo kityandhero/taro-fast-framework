a9e3be08b92d8881b49e1879c6e0430cecbfeb24
卢志涛 <kityandhero@126.com>
2022-04-16 15:57:24 +0800

    refactor(taro-fast-common): update

--- packages/simple-taro-fast-framework/src/example/pages/ImageBox/index.jsx
+++ packages/simple-taro-fast-framework/src/example/pages/ImageBox/index.jsx
@@ -40,10 +40,10 @@ const imageContainerStyle = {
   backgroundColor: 'red',
 };
 
-// const src =
-//   'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.jj20.com%2Fup%2Fallimg%2Ftp09%2F21052112102250D-0-lp.jpg&refer=http%3A%2F%2Fimg.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1643989392&t=18546318aa0f8e3e126ab26965ca6f45';
+const src =
+  'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.jj20.com%2Fup%2Fallimg%2Ftp09%2F21052112102250D-0-lp.jpg&refer=http%3A%2F%2Fimg.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1643989392&t=18546318aa0f8e3e126ab26965ca6f45';
 
-const src = 'https://file.1010101.cc/2129867544.png';
+// const src = 'https://file.1010101.cc/2129867544.png';
 
 function wrapBuilder(one) {
   return <View style={imageBoxContainerStyle}>{one}</View>;
