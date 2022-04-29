6384792771956dad894611b68f62fa9f0a6383ff
kityandhero <kityandhero@126.com>
2022-04-29 12:18:25 +0800

    refactor(taro-fast-common): update

--- packages/taro-fast-framework/src/utils/globalStorageAssist.js
+++ packages/taro-fast-framework/src/utils/globalStorageAssist.js
@@ -45,6 +45,7 @@ export const storageKeyCollection = {
   modelNameList: 'modelNameList',
   metaData: 'metaData',
   administrativeDivisionFullData: 'administrativeDivisionFullData',
+  selectedAddressData: 'selectedAddressData',
 };
 
 export function getNearestLocalhostNotifyCache() {
@@ -1133,6 +1134,51 @@ export function removeAdministrativeDivisionFullData() {
   recordDebug('exec removeAdministrativeDivisionFullData');
 
   const key = storageKeyCollection.administrativeDivisionFullData;
+
+  removeLocalStorage(key);
+}
+
+/**
+ * 获取已选择的地址
+ *
+ * @export
+ * @param {*} fn
+ * @returns
+ */
+export function getSelectedAddressData() {
+  recordDebug('exec getSelectedAddressData from local cache');
+
+  const key = storageKeyCollection.selectedAddressData;
+
+  return getJsonFromLocalStorage(key) || null;
+}
+
+/**
+ * 设置已选择的地址
+ *
+ * @export
+ * @param {*} fn
+ * @returns
+ */
+export function setSelectedAddressData(data) {
+  recordDebug('exec setSelectedAddressData to local cache');
+
+  const key = storageKeyCollection.selectedAddressData;
+
+  return saveJsonToLocalStorage(key, data || '');
+}
+
+/**
+ * 移除已选择的地址
+ *
+ * @export
+ * @param {*} fn
+ * @returns
+ */
+export function removeSelectedAddressData() {
+  recordDebug('exec removeSelectedAddressData');
+
+  const key = storageKeyCollection.selectedAddressData;
 
   removeLocalStorage(key);
 }
