db0b5a603e737df8a92990635fd5ed3c7f0459f2
kityandhero <kityandhero@126.com>
2022-06-06 11:55:16 +0800

    refactor(simple-taro-fast-framework): update

--- packages/taro-fast-component/src/customComponents/ImageBox/index.jsx
+++ packages/taro-fast-component/src/customComponents/ImageBox/index.jsx
@@ -19,7 +19,7 @@ const classPrefix = `tfc-image-box`;
 const defaultProps = {
   src: '',
   padding: 0,
-  lazyLoad: false,
+  lazyLoad: true,
   aspectRatio: 1,
   imageBoxStyle: {},
   borderRadius: true,
