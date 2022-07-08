82459ffded5f8ecf6b92461e20e28768fca4debd
kityandhero <kityandhero@126.com>
2022-07-08 17:10:43 +0800

    refactor(simple-taro-fast-framework): update

--- packages/taro-fast-component/src/customComponents/HtmlBox/index.jsx
+++ packages/taro-fast-component/src/customComponents/HtmlBox/index.jsx
@@ -5,16 +5,12 @@ import { isFunction } from 'taro-fast-common/es/utils/typeCheck';
 
 import BaseComponent from '../BaseComponent';
 
-import './index.less';
-
-const classPrefix = `tfc-html-box`;
-
 if (process.env.TARO_ENV !== 'h5') {
   require('@tarojs/taro/html.css');
 
   Taro.options.html.transformElement = (el) => {
     if (el.nodeName === 'image') {
-      el.setAttribute('class', `${classPrefix}-image`);
+      el.setAttribute('mode', 'widthFix');
     }
     return el;
   };
